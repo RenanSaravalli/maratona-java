f3a03f0a3854862e71f3d798b521e486dfe241bd
RenanSaravalli <renansaravalli@gmail.com>
2025-03-31 19:20:35 -0300

    47 - Orientação Objetos - Métodos pt 04 - Retorno pt 02 "Usando diferentes lógicas de utilização do return" em métodos

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/teste/CalculadoraTeste03.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/teste/CalculadoraTeste03.java
@@ -8,5 +8,13 @@ public class CalculadoraTeste03 {
 
         double resultado = calculadora.divideDoisNumeros(10, 2);
         System.out.println(resultado);
+
+        System.out.println(calculadora.divideDoisNumeros(10,0));
+        System.out.println(calculadora.divideDoisNumeros02(10,0));
+
+        System.out.println("----------------");
+
+        calculadora.imprimeDivisaoDoisNumeros(20,0);
+        calculadora.imprimeDivisaoDoisNumeros(86,5);
     }
 }
