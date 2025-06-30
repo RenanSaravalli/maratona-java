60e8506a04f4544878d89534db0e235e239fdaa4
RenanSaravalli <renansaravalli@gmail.com>
2025-06-29 22:12:22 -0300

    68 - Utilizando o scanner para receber um dado do usuário pelo console

---
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/teste/LeituraDoTecladoTest01.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.Gassociacao.teste;
+
+import java.util.Scanner;
+
+public class LeituraDoTecladoTest01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // scanner para leitura de dados do usuário
+        String opa = scanner.nextLine();
+        System.out.println(opa);
+
+    }
+}
