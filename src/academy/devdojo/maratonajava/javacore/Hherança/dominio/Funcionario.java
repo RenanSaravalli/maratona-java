45c20d5205c0f8c921f2b3544ffdb4700bd9a5a8
RenanSaravalli <renansaravalli@gmail.com>
2025-07-01 19:28:11 -0300

    75 - Herança pt05 entendo como vai ser o bloco de inicialização com o relacionamento de herança entre as classes

--- src/academy/devdojo/maratonajava/javacore/Hherança/dominio/Funcionario.java
+++ src/academy/devdojo/maratonajava/javacore/Hherança/dominio/Funcionario.java
@@ -2,9 +2,18 @@ package academy.devdojo.maratonajava.javacore.Hherança.dominio;
 
 public class Funcionario extends Pessoa{
     private double salario;
-
+    static {
+        System.out.println("Dentro do bloco de inicialização estático de funcionário");
+    }
+    {
+        System.out.println("Dentro do bloco de inicialização de funcionário 1");
+    }
+    {
+        System.out.println("Dentro do bloco de inicialização de funcionário 2");
+    }
     public Funcionario(String nome) {
         super(nome);
+        System.out.println("Dentro do construtor de funcionário");
     }
 
 
---
+++ src/academy/devdojo/maratonajava/javacore/Hherança/teste/HerancaTeste02.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonajava.javacore.Hherança.teste;
+
+import academy.devdojo.maratonajava.javacore.Hherança.dominio.Funcionario;
+
+public class HerancaTeste02 {
+    /* Ordem da classe
+     *  0 - Bloco de inicialização estático da super classe é executado quando a JVM carregar a classe pai
+     *  1 - Bloco de inicialização estático da sub classe é executado quando a JVM carregar a classe filha
+     *  2 - Alocado espaço em memória pro objeto da superclasse
+     *  3 - Cada atributo da super classe é criado e inicializado com valores default ou o que for passado
+     *  4 - Bloco de inicialização da superclasse é inicializado na ordem que aparece
+     *  5 - Construtor é executado da superclasse
+     *  6 - Alocado espaço em memória pro objeto da subclasse
+     *  7 - Cada atributo de subclasse é criado e inicializado com valores default ou oque for passado
+     *  8 - Bloco de inicialização da subclasse é executado na ordem em que aparece
+     *  9 - Construtor é executado da subclasse */
+
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario("REnan");
+    }
+}
