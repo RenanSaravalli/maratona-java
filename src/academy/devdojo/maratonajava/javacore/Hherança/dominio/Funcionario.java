3e102e0d8969146ad0ca984b2cfc510712ca3898
RenanSaravalli <renansaravalli@gmail.com>
2025-07-01 18:00:34 -0300

    71 - Herança pt02 aprendendo a utilizar o super para puxar as funcionalidades de um método vindo da classe mãe que está sendo sobrescrito

--- src/academy/devdojo/maratonajava/javacore/Hherança/dominio/Funcionario.java
+++ src/academy/devdojo/maratonajava/javacore/Hherança/dominio/Funcionario.java
@@ -3,6 +3,12 @@ package academy.devdojo.maratonajava.javacore.Hherança.dominio;
 public class Funcionario extends Pessoa{
     private double salario;
 
+    public void imprime(){
+        System.out.println("Eu sou um funcionário");
+        super.imprime();
+        System.out.println("EU ganho R$"+this.salario);
+    }
+
     public double getSalario() {
         return salario;
     }
