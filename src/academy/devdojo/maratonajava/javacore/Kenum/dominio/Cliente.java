5f2a597f23c7e15516fbca8a8de97df757d57754
RenanSaravalli <renansaravalli@gmail.com>
2025-07-02 21:08:20 -0300

    81 - Orientação Objetos - Enumeração pt 02 - Construtores e atributos

--- src/academy/devdojo/maratonajava/javacore/Kenum/dominio/Cliente.java
+++ src/academy/devdojo/maratonajava/javacore/Kenum/dominio/Cliente.java
@@ -19,6 +19,8 @@ public class Cliente {
     public String toString() {
         return "Cliente{" +
                 "nome='" + nome + '\'' +
+                ", tipoCliente=" + tipoCliente +
+                ", tipoClienteInt=" + tipoCliente.VALOR +
                 ", tipoPagamento=" + tipoPagamento +
                 '}';
     }
