0146ded8b98f91045516504e9a19fbb789751585
RenanSaravalli <renansaravalli@gmail.com>
2025-07-03 19:59:28 -0300

    84 - Orientação Objetos - Classes abstratas pt 01

---
+++ src/academy/devdojo/maratonajava/javacore/Lclassesabstratas/teste/FuncionarioTeste01.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.javacore.Lclassesabstratas.teste;
+
+import academy.devdojo.maratonajava.javacore.Lclassesabstratas.dominio.Desenvolvedor;
+import academy.devdojo.maratonajava.javacore.Lclassesabstratas.dominio.Funcionario;
+import academy.devdojo.maratonajava.javacore.Lclassesabstratas.dominio.Gerente;
+
+public class FuncionarioTeste01 {
+    public static void main(String[] args) {
+        Gerente gerente = new Gerente("Luffy", 6000);
+        System.out.println(gerente);
+
+        Desenvolvedor desenvolvedor = new Desenvolvedor("Toya", 21000);
+        System.out.println(desenvolvedor);
+    }
+}
