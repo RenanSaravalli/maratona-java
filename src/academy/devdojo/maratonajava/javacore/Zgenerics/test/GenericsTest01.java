dea99fd869487f37c729067afb6aee343aecd929
RenanSaravalli <renansaravalli@gmail.com>
2025-07-24 16:28:10 -0300

    183 - Generics pt 01 - Introdução

---
+++ src/academy/devdojo/maratonajava/javacore/Zgenerics/test/GenericsTest01.java
@@ -0,0 +1,24 @@
+package academy.devdojo.maratonajava.javacore.Zgenerics.test;
+
+public class GenericsTest01 {
+    public static void main(String[] args) {
+/*        Vamos iniciar aqui entendendo o conceito para que existe
+
+        Surgiu com a necessidade de definir um tipo específico para a lista, pense que antigamente no java 1.4,
+         podia colocar na lista vários tipos de valores em conjunto, o que não fazia sentido e atrapalhava na lógica.
+
+        Então surgiu a necessidade de implementar essa lógica de tipar as coleções, mas eles não queriam implementar
+        algo que quebrasse todos aqueles sistemas anteriores, pq a idéia do java é dar suporte para todos os tipos de sistemas,
+         desde dos mais antigos até os mais atuais.
+
+        Foi aí que eles vieram com a solução de utilizar em tempo de compilação (Generics é algo que foi adicionado em tempo de compilação )
+
+        Esse conceito é chamado de `Type erasure`, pois ele está apagando o tipo depois que o código é compilado,
+        isso para dar compatibilidade para todos tipos de sistemas
+
+        O uso do generics é importante pois ele vai fazer essa validação em tempo de compilação, ou seja,
+         ele não deixa seu código passar para JVM, se você não tomar cuidado e passar o tipo correto esperado
+
+        Então o Generics foi criado realmente para melhorar essa validação de tipos*/
+    }
+}
