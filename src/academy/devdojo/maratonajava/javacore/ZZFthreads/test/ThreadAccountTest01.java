41d6f62b445a8b2a1216149f260c0d397d53a67e
RenanSaravalli <renansaravalli@gmail.com>
2025-08-03 10:31:08 -0300

    225 - Threads pt 06 - Sincronismo de thread pt 02

---
+++ src/academy/devdojo/maratonajava/javacore/ZZFthreads/test/ThreadAccountTest01.java
@@ -0,0 +1,43 @@
+package academy.devdojo.maratonajava.javacore.ZZFthreads.test;
+
+import academy.devdojo.maratonajava.javacore.ZZFthreads.dominio.Account;
+
+// vamos implementar o Runnable para rodar nossas threads
+public class ThreadAccountTest01 implements Runnable{
+
+    private Account account = new Account();
+
+    public static void main(String[] args) {
+        ThreadAccountTest01 threadAccountTest01 = new ThreadAccountTest01();
+        Thread t1 = new Thread(threadAccountTest01, "Hestia");
+        Thread t2 = new Thread(threadAccountTest01, "Bell Cranel");
+        t1.start();
+        t2.start();
+        // O maior problema da concorrência. Ter 2 Threads ou mais
+        // acessando o mesmo objeto
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            withdrawal(10);
+            if (account.getBalance() < 0) {
+                System.out.println("FODEU");
+            }
+        }
+    }
+
+    private void withdrawal(int amount) {
+        if (account.getBalance() >= amount) {
+            System.out.println(getThreadName() + " Está indo sacar dinheiro");
+            account.withdrawal(amount);
+            System.out.println(getThreadName() + " completou o saque, valor atual da conta: " + account.getBalance());
+        } else {
+            System.out.println("Sem dinheiro para " + getThreadName() + " efetuar o saque " + account.getBalance());
+        }
+    }
+
+    private static String getThreadName() {
+        return Thread.currentThread().getName();
+    }
+}
