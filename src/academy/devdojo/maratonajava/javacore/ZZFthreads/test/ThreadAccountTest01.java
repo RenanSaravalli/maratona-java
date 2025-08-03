e68b3a0e06ebbe753df77dad9580e54747b83c74
RenanSaravalli <renansaravalli@gmail.com>
2025-08-03 11:13:26 -0300

    225 - Threads pt 06 - Sincronismo de thread pt 02

--- src/academy/devdojo/maratonajava/javacore/ZZFthreads/test/ThreadAccountTest01.java
+++ src/academy/devdojo/maratonajava/javacore/ZZFthreads/test/ThreadAccountTest01.java
@@ -3,14 +3,14 @@ package academy.devdojo.maratonajava.javacore.ZZFthreads.test;
 import academy.devdojo.maratonajava.javacore.ZZFthreads.dominio.Account;
 
 // vamos implementar o Runnable para rodar nossas threads
-public class ThreadAccountTest01 implements Runnable{
+public class ThreadAccountTest01 implements Runnable {
 
-    private Account account = new Account();
+    private final Account account = new Account();
 
     public static void main(String[] args) {
         ThreadAccountTest01 threadAccountTest01 = new ThreadAccountTest01();
-        Thread t1 = new Thread(threadAccountTest01, "Hestia");
-        Thread t2 = new Thread(threadAccountTest01, "Bell Cranel");
+        Thread t1 = new Thread(threadAccountTest01, "thread: Hestia");
+        Thread t2 = new Thread(threadAccountTest01, "thread: Bell Cranel");
         t1.start();
         t2.start();
         // O maior problema da concorrência. Ter 2 Threads ou mais
@@ -27,13 +27,21 @@ public class ThreadAccountTest01 implements Runnable{
         }
     }
 
+    // Para resvolver o problema temos que fazer basicamente
+    // a thread que começar o métod tem que executa-lo
+    // completamente, terminar de executar
+    // ai sim ouutra thread pode executar esse métod
     private void withdrawal(int amount) {
-        if (account.getBalance() >= amount) {
-            System.out.println(getThreadName() + " Está indo sacar dinheiro");
-            account.withdrawal(amount);
-            System.out.println(getThreadName() + " completou o saque, valor atual da conta: " + account.getBalance());
-        } else {
-            System.out.println("Sem dinheiro para " + getThreadName() + " efetuar o saque " + account.getBalance());
+        System.out.println(getThreadName() + " ###### fora do synchronized");
+        synchronized (account) {
+            System.out.println(getThreadName() + " ***** dentro do synchronized");
+            if (account.getBalance() >= amount) {
+                System.out.println(getThreadName() + " Está indo sacar dinheiro");
+                account.withdrawal(amount);
+                System.out.println(getThreadName() + " completou o saque, valor atual da conta: " + account.getBalance());
+            } else {
+                System.out.println("Sem dinheiro para " + getThreadName() + " efetuar o saque " + account.getBalance());
+            }
         }
     }
 
